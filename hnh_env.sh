5a467739ab6284558793727e726b65e77066edd5
Nathan Haberman <nathanhaberman@localhost.localdomain>
2018-10-31 14:37:41 -0500

    Start project

---
+++ hnh_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+source hnh_env/bin/activate
---
+++ hnh_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nathanhaberman/Development/hideouts_and_hoodlums/hnh_env/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
