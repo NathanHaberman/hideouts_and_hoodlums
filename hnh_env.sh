824a686c2971b009a0ffb607e83712237d9f87f6
Nathan Haberman <nathanhaberman@localhost.localdomain>
2018-11-26 14:45:27 -0600

    Project pivot

--- hnh_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-source hnh_env/bin/activate
--- server/apps/login/urls.py
+++ server/apps/login/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from views import *
 
 urlpatterns = [
-    #url(r'^$', inbox),
+    # url(r'^$', inbox),
+    url(r'^$', index),
 ]
