fbf7ee1e2f292fe191953e34057da0930479e407
Nathan Haberman <na.haberman@gmail.com>
2018-12-28 17:01:04 -0600

    Testing if commit shows on GitHub

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^', include("apps.player.urls")),
     url(r'^hideout/', include('apps.hideout.urls')),
